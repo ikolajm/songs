3979225f247ef5e09df036c46afbb07cc6d09d53
Jake Ikola <ikolajm@gmail.com>
2018-12-12 12:16:00 -0500

    Initial Commit
    finished small project to learn about react-redux

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { selectSong } from '../actions/index.js';
+import SongList from './SongList.js';
+import SongDetail from './SongDetail.js';
+
+const App = () => {
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
